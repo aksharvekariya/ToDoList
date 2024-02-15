35f5d31337ceb0bd534636c2345f831786d28d01
aksharvekariya <157659615+aksharvekariya@users.noreply.github.com>
2024-02-15 19:06:24 +0000

    Akshar

---
+++ TodoList.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using TodoListAPI;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoListAPI
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+    }
+}
